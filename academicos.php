fb9147fcc6605d6eb05d06e14ec393396b3f8d4c
Egmon Pereira <egmonpereira@gmail.com>
2021-07-01 08:16:07 -0300

    Add files via upload
    Reposicionamento de links

--- academicos.php
+++ academicos.php
@@ -51,28 +51,6 @@ $image_rand = "imagens/bg(".$rand.").jpg";
     <!--img width="610"  height="170" src="javascript:banners();"><br-->
 	<!--REDES SOCIAIS-->
 
-  <div class="container">
-    <a target="_blank" href="https://www.projetocaveira.com.br/novo-sistema"><img width="80" src="imagens/caveira.png"></a>
-    <a target="_blank" href="https://extensivoprf2020.club.hotmart.com/"><img width="80" src="imagens/extensivo-prf.png"></a>
-    <a target="_blank" href="https://projetoretafinalavancado.club.hotmart.com/"><img width="80" src="imagens/qb.png"></a>
-    <a target="_blank" href="https://www.aprovaconcursos.com.br"><img width="80" src="imagens/aprova.png"></a>
-    <a target="_blank" href="https://ava.aprovaconcursos.com.br"><img width="80" src="imagens/aprovaava.png"></a>
-    <a target="_blank" href="https://www.qconcursos.com/"><img width="80" src="imagens/qconcursos.png"></a>
-    <a target="_blank" href="https://www.estrategiaconcursos.com.br/"><img width="80" src="imagens/estrategia.png"></a>
-    <a target="_blank" href="https://www.pciconcursos.com.br/"><img width="80" src="imagens/PCI-Concursos.png"></a>
-    <a target="_blank" href="http://www.timoteo.cefetmg.br/"><img width="80" src="imagens/cefet.png"></a>
-  </div>
-
-  <div class="container">
-    <a target="_blank" href="https://sig.cefetmg.br/sigaa/"><img width="80" src="imagens/sigaa.png"></a>
-    <a target="_blank" href="https://ava.cefetmg.br/login/index.php"><img width="80" src="imagens/moodle.png"></a>
-    <a target="_blank" href="https://sistemas.ufmg.br/portal/render.userLayoutRootNode.uP"><img width="80" src="imagens/ufmg.png"></a>
-    <a target="_blank" href="https://convertio.co/pt/"><img width="80" src="imagens/convertio.png"></a>
-    <a target="_blank" href="https://cognitiveclass.ai"><img width="80" src="imagens/cognitive.png"></a>
-    <a target="_blank" href="https://www.ead.cesumar.br/site/"><img width="80" src="imagens/unicesumar.png"></a>
-    <a target="_blank" href="https://www.bibliotecadaengenharia.com/"><img width="80" src="imagens/bibliotecadaengenharia.png"></a>
-    <a target="_blank" href="https://www.overleaf.com"><img width="80" src="imagens/overleaf.png"></a>
-  </div>
 
   <div class="container">
     <a target="_blank" href="https://www.urionlinejudge.com.br/judge/pt"><img width="80" src="imagens/uri.png"></a>
@@ -83,6 +61,29 @@ $image_rand = "imagens/bg(".$rand.").jpg";
     <a target="_blank" href="https://www.cebraspe.org.br/concursos/prf_21"><img width="80" src="imagens/brasaoprf.png"></a>
     <a target="_blank" href="http://portal.nc.ufpr.br/PortalNC/Login"><img width="80" src="imagens/pcpr.png"></a>
     <a target="_blank" href="https://www.cebraspe.org.br/concursos/PC_DF_19_ESCRIVAO"><img width="80" src="imagens/pcdf.png"></a>
+  </div>
+
+  <div class="container">
+    <a target="_blank" href="http://www.timoteo.cefetmg.br/"><img width="80" src="imagens/cefet.png"></a>
+    <a target="_blank" href="https://sig.cefetmg.br/sigaa/"><img width="80" src="imagens/sigaa.png"></a>
+    <a target="_blank" href="https://ava.cefetmg.br/login/index.php"><img width="80" src="imagens/moodle.png"></a>
+    <a target="_blank" href="https://sistemas.ufmg.br/portal/render.userLayoutRootNode.uP"><img width="80" src="imagens/ufmg.png"></a>
+    <a target="_blank" href="httphp.validate.executablePathps://convertio.co/pt/"><img width="80" src="imagens/convertio.png"></a>
+    <a target="_blank" href="https://cognitiveclass.ai"><img width="80" src="imagens/cognitive.png"></a>
+    <a target="_blank" href="https://www.ead.cesumar.br/site/"><img width="80" src="imagens/unicesumar.png"></a>
+    <a target="_blank" href="https://www.bibliotecadaengenharia.com/"><img width="80" src="imagens/bibliotecadaengenharia.png"></a>
+    <a target="_blank" href="https://www.overleaf.com"><img width="80" src="imagens/overleaf.png"></a>
+  </div>
+
+  <div class="container">
+    <a target="_blank" href="https://www.projetocaveira.com.br/novo-sistema"><img width="80" src="imagens/caveira.png"></a>
+    <a target="_blank" href="https://extensivoprf2020.club.hotmart.com/"><img width="80" src="imagens/extensivo-prf.png"></a>
+    <a target="_blank" href="https://projetoretafinalavancado.club.hotmart.com/"><img width="80" src="imagens/qb.png"></a>
+    <a target="_blank" href="https://www.aprovaconcursos.com.br"><img width="80" src="imagens/aprova.png"></a>
+    <a target="_blank" href="https://ava.aprovaconcursos.com.br"><img width="80" src="imagens/aprovaava.png"></a>
+    <a target="_blank" href="https://www.qconcursos.com/"><img width="80" src="imagens/qconcursos.png"></a>
+    <a target="_blank" href="https://www.estrategiaconcursos.com.br/"><img width="80" src="imagens/estrategia.png"></a>
+    <a target="_blank" href="https://www.pciconcursos.com.br/"><img width="80" src="imagens/PCI-Concursos.png"></a>
     <a href="index.php" target="_self"><img width="80" src="imagens/inicio3.png"></a>
   </div>
    </center>
