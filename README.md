d4424cb02293b9f6a2818c5c6462aecf27e2a2f4
egmonpereira <egmonpereira@gmail.com>
2021-09-11 16:49:57 -0300

    Atualização README.md, tamanho de imagens-links, inserção de versão nas páginas, correção de extensão de imagens

---
+++ README.md
@@ -0,0 +1,5 @@
+# Site_Pessoal
+
+Meu site pessoal que uso no meu computador para facilitar acesso a vários sites que uso no dia a dia.
+
+Versão: 21.09.11
--- index.php
+++ index.php
@@ -109,5 +109,8 @@
           </tr> 
         </table>
       </center>
+      <center>
+        <font size="-3" color="#fff">egmon@ufmg.br - 2021 - Versão: 21.09.11</font>
+      </center>
     </body>
   </html>
